2d6ff02fb066709ae86b604ab71cadc956e90ee3
Craig Forrest <craforrest97@gmail.com>
2026-02-07 16:18:51 +0000

    Update role_policy_arns to use AdministratorAccess for GitHub OIDC module

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "github_oidc" {
   role_name = "github-actions-infra-role"
 
   role_policy_arns = [
-    "arn:aws:iam::aws:policy/PowerUserAccess"
+    "arn:aws:iam::aws:policy/AdministratorAccess"
   ]
 
   tags = local.common_tags
